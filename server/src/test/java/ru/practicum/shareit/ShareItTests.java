d9ad1352dee4ec1917e3de22c58df605fb3bb521
igrshrn <igrshrn@yandex.ru>
2025-07-21 00:46:02 +0300

    Reapply "Fix#2-Изменение логики проверки валидации"
    This reverts commit 64a3b85c70ab6ac8a597561d77924aea9e3a4a3a.

--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,13 +1,35 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ShareItTests {
+    @Autowired
+    private ApplicationContext context;
+
+    @Value("${spring.main.banner-mode}")
+    private String bannerMode;
 
     @Test
     void contextLoads() {
+        assertNotNull(context);
+    }
+
+    @Test
+    void verifyMainBeansAreAvailable() {
+        assertNotNull(context.getBean(ShareItServer.class));
+    }
+
+    @Test
+    void testCustomProperty() {
+        assertEquals("off", bannerMode);
     }
 
 }
